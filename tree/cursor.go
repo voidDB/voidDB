53d8e3c07bb709c828be90077791a0f179a37eb9
Joel Ling <joel-ling@protonmail.com>
2024-12-30 15:33:59 +0900

    adjust & justify maximum cursor stack depth

--- tree/cursor.go
+++ tree/cursor.go
@@ -11,7 +11,11 @@ type Cursor struct {
 
 func NewCursor(medium Medium, offset int) *Cursor {
 	const (
-		maxStackDepth = 512
+		maxStackDepth = 26 // = log4(2^64 / 4096)
+		// Assuming MaxNodeLength = 7 (such that each node must have at least
+		// four children) and PageSize = 4096, a stack depth of 26 would put
+		// within reach every node that could ever exist in a 64-bit address
+		// space. 18 would suffice for the more common 48-bit address space.
 	)
 
 	return &Cursor{
